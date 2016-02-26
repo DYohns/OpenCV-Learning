2b70a0d2beb0c4befae32c0190890166aaae3e28
DYohns <dychayohns@outlook.com>
2016-02-26 10:52:30 +0800

    MODIFY
    fastest function

--- ColorReduce.cpp
+++ ColorReduce.cpp
@@ -1,61 +1,59 @@
 ﻿#include<opencv2/core/core.hpp>
 #include<opencv2/highgui/highgui.hpp>
 #include<string>
-
+#include<iostream>
 /*
  fn: color reduce
- using mask to round pixel value
+ using .ptr and * ++ and bitwise (continuous+channels)
 */
-void ColorReduce(cv::Mat &img, cv::Mat &result_img, int div = 64) {
-	result_img.create(img.rows, img.cols, img.type());
+void ColorReduce(cv::Mat &image, int div = 64) {
 
-	int nl = img.rows; // number of lines				   
-	int nc = img.cols * img.channels(); // total number of elements per line
+	int nl = image.rows; // number of lines
+	int nc = image.cols; // number of columns
+
+	if (image.isContinuous()) {
+		// then no padded pixels
+		nc = nc*nl;
+		nl = 1;  // it is now a 1D array
+	}
 
 	int n = static_cast<int>(log(static_cast<double>(div)) / log(2.0));
-	// mask used to round pixel value
-	uchar mask = 0xFF << n;
+	// mask used to round the pixel value
+	uchar mask = 0xFF << n; // e.g. for div=16, mask= 0xF0
 
 	for (int j = 0; j < nl; j++) {
-		// get jth first address
-		uchar* data_img = img.ptr<uchar>(j);
-		uchar* data_ri = result_img.ptr<uchar>(j);
+
+		uchar* data = image.ptr<uchar>(j);
+
 		for (int i = 0; i < nc; i++) {
 
-			// process each pixel
-			data_ri[i] = (data_img[i] & mask) + div / 2;
-			//data[i] = data[i] / div * div + div / 2;
-		}
-	}
-}
+			// process each pixel ---------------------
 
-/*
- fn: color reduce
- using Mat_ iterator
-*/
-void ColorReduce1(cv::Mat &img, cv::Mat &result_img, int div = 64) {
-	result_img = img.clone();
+			*data++ = *data&mask + div / 2;
+			*data++ = *data&mask + div / 2;
+			*data++ = *data&mask + div / 2;
 
-	// get iterators
-	cv::Mat_<cv::Vec3b>::iterator it = result_img.begin<cv::Vec3b>();
-	cv::Mat_<cv::Vec3b>::const_iterator itend = result_img.end<cv::Vec3b>();
+			// end of pixel processing ----------------
 
-	for (; it != itend; ++it) {
-		// process each pixel
-		(*it)[0] = (*it)[0] / div * div + div / 2;
-		(*it)[1] = (*it)[1] / div * div + div / 2;
-		(*it)[2] = (*it)[2] / div * div + div / 2;
-		// end of pixel processing
+		} // end of line                   
 	}
 }
 
 int main() {
 	std::string str_winName = "Image";
-	cv::Mat img = cv::imread("Images/boldt.jpg");
-	cv::Mat result_img;
-	ColorReduce1(img, result_img);
+	cv::Mat image = cv::imread("Images/boldt.jpg");
+
+	double duration;
+	duration = static_cast<double>(cv::getTickCount());
+
+	ColorReduce(image);
+
+	duration = static_cast<double>(cv::getTickCount()) - duration;
+	duration /= cv::getTickFrequency();
+	std::cout << duration << std::endl;
+
 	cv::namedWindow(str_winName);
-	cv::imshow(str_winName, result_img);
+	cv::imshow(str_winName, image);
 	cv::waitKey(0);
 	return 1;
 }
